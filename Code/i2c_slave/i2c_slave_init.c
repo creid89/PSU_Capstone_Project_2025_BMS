9698b67b57efd507f55cdd31b9859c12382f56f4
creid89 <152050857+creid89@users.noreply.github.com>
2025-02-01 18:05:32 -0800

    Add files via upload

--- Code/i2c_slave/i2c_slave_init.c
+++ Code/i2c_slave/i2c_slave_init.c
@@ -8,12 +8,14 @@
 #define I2C_PORT i2c1 //using i2c1 for comms on current sensor and ADC IC
 #define SDA_PIN 4  //GPIO6 / D4 / SDA1
 #define SCL_PIN 5 //GPIO7 / D5 / SCL1
-#define I2C_FREQ 100000 //100kHz
+#define I2C_FREQ 9600 //100kHz
 #define INA219_I2C_ADDR 0x40 //INA219 Default I2C Address
 #define ADS7830_I2C_ADDR 0x48 //ADS7830 Base Address
 #define I2C_SLAVE_ADDR 0x08 //to be used with slave init of RP2350
 
 #define LED_PIN 25
+
+volatile uint8_t slave_buffer[10] = {0xAA, 0xBB, 0xCC, 0xDD}; // Example response
 
 // Initialize the GPIO for the LED
 void pico_led_init(void) {
@@ -33,6 +35,30 @@ void pico_set_led(bool led_on) {
 #endif
 }
 
+// Function to handle incoming I2C requests
+void i2c_slave_handler() {
+        while(1){
+        // Check if master is writing data
+        while (i2c_get_read_available(I2C_PORT)) {
+            uint8_t received_byte = i2c_read_byte_raw(I2C_PORT);
+            printf("Received: 0x%02X\n", received_byte);
+        }
+        pico_set_led(true);
+        printf("LED ON\n");
+        sleep_ms(500);
+        pico_set_led(false);
+        printf("LED OFF\n");
+        sleep_ms(500);
+
+        // Check if master is requesting data
+        while (i2c_get_write_available(I2C_PORT)) {
+            i2c_write_byte_raw(I2C_PORT, slave_buffer[0]); // Always send first byte
+            printf("Sent: 0x%02X\n", slave_buffer[0]);
+        }        
+    }
+
+}
+
 void init_i2c() {
 
     i2c_init(I2C_PORT, I2C_FREQ);
@@ -48,7 +74,6 @@ void init_i2c() {
     printf("\nInit in slave mode\n");
     i2c_set_slave_mode(I2C_PORT, true, I2C_SLAVE_ADDR );
     printf("");
-
 
 }
 
@@ -80,7 +105,9 @@ int main() {
 
     //send_i2c_data(0xFF, INA219_I2C_ADDR);
 
-    
+    i2c_slave_handler();
+
+
 
     while(1){
         pico_set_led(true);
